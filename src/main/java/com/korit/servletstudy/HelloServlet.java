7f97a30ff114659f22d27ac0d99348ce9535c22f
code1218 <skjil1218@gmail.com>
2026-04-10 17:51:00 +0900

    서블릿 초기화

---
+++ src/main/java/com/korit/servletstudy/HelloServlet.java
@@ -0,0 +1,29 @@
+package com.korit.servletstudy;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("<h1>" + "김준일" + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
